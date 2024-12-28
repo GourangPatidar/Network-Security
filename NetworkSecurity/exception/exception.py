8f5443367006a8d78f288491433f6191d1022852
Gourang Patidar <goourangpatidar2003@gmail.com>
2024-12-28 21:28:20 +0530

    ETL Pipeline completed

--- NetworkSecurity/exception/exception.py
+++ NetworkSecurity/exception/exception.py
@@ -13,22 +13,5 @@ class NetworkSecurityException(Exception):
         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         self.file_name, self.lineno, str(self.error_message))
         
-if __name__=='__main__':
-    try:
-        logger.logging.info("Enter the try block")
-        a=1/0
-        print("This will not be printed",a)
-    except Exception as e:
-           raise NetworkSecurityException(e,sys)
-    
 
-
-if __name__=="__main__":
-
-    try :
-          logger.logging.INFO("enter the try block")
-          a=1/0
-          print(a)
-    except Exception as e:
-         print(e)
     
