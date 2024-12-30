98788708ac96f171476d864a39f1150fb6a0df3e
Gourang Patidar <goourangpatidar2003@gmail.com>
2024-12-30 21:18:46 +0530

    updated

--- NetworkSecurity/components/model_trainer.py
+++ NetworkSecurity/components/model_trainer.py
@@ -1,18 +1,18 @@
 import os
 import sys
 
-from networksecurity.exception.exception import NetworkSecurityException 
-from networksecurity.logging.logger import logging
+from NetworkSecurity.exception.exception import NetworkSecurityException 
+from NetworkSecurity.logging.logger import logging
 
-from networksecurity.entity.artifact_entity import DataTransformationArtifact,ModelTrainerArtifact
-from networksecurity.entity.config_entity import ModelTrainerConfig
+from NetworkSecurity.entity.artifact_entity import DataTransformationArtifact,ModelTrainerArtifact
+from NetworkSecurity.entity.config_entity import ModelTrainerConfig
 
 
 
-from networksecurity.utils.ml_utils.model.estimator import NetworkModel
-from networksecurity.utils.main_utils.utils import save_object,load_object
-from networksecurity.utils.main_utils.utils import load_numpy_array_data,evaluate_models
-from networksecurity.utils.ml_utils.metric.classification_metric import get_classification_score
+from NetworkSecurity.utils.ml_utils.model.estimator import NetworkModel
+from NetworkSecurity.utils.main_utils.utils import save_object,load_object
+from NetworkSecurity.utils.main_utils.utils import load_numpy_array_data,evaluate_models
+from NetworkSecurity.utils.ml_utils.metric.classification_metric import get_classification_score
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import r2_score
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from NetworkSecurity.components.data_ingestion import DataIngestion
 from NetworkSecurity.components.data_validation import DataValidation
 from NetworkSecurity.components.data_transformation import DataTransformation
+from NetworkSecurity.components.data_trainer import DataT
 
 from NetworkSecurity.exception.exception import NetworkSecurityException
 from NetworkSecurity.logging.logger import logging
@@ -32,5 +33,11 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
     except NetworkSecurityException as e:
         raise NetworkSecurityException(e, sys)
